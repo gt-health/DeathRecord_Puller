79ba30b008ee9da95b46207963d0c3f430c4b82b
Riley <mriley7@icl.gtri.org>
2018-01-05 18:05:32 -0500

    scanning for ECR repositories now.

--- src/main/java/gatech/edu/FHIRController/ApplicationTest.java
+++ src/main/java/gatech/edu/FHIRController/ApplicationTest.java
@@ -8,13 +8,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
+@EntityScan("gatech.edu.STIECR.DB.model")
+@EnableJpaRepositories("gatech.edu.STIECR.DB.repo")
 @ComponentScan("gatech.edu.common.FHIR.client")
 @ComponentScan("gatech.edu.FHIRController.Controller")
 @ComponentScan("gatech.edu.FHIRController.PHCRClient")
